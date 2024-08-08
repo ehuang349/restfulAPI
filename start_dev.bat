e29c5ab94d1650ea6ec65c82c240d41e6ec5b0af
Eric <ehuang349@gmail.com>
2024-08-08 08:03:07 -0400

    Adjusted the .bat scripts.

--- start_dev.bat
+++ start_dev.bat
@@ -2,5 +2,9 @@
 REM Set environment variables from the .env file
 for /f "tokens=1,2 delims==" %%a in (environments/dev.env) do set %%a=%%b
 
+REM kill existing uvicorn process
+for /f "tokens=5" %%a in ('netstat -ano ^| findstr :8000') do taskkill /f /pid %%a
+
+
 REM Start the FastAPI application
 python main.py
--- start_prod.bat
+++ start_prod.bat
@@ -2,5 +2,9 @@
 REM Set environment variables from the .env file
 for /f "tokens=1,2 delims==" %%a in (environments/prod.env) do set %%a=%%b
 
+REM Kill the existing Uvicorn process
+for /f "tokens=5" %%a in ('netstat -ano ^| findstr :8000') do taskkill /f /pid %%a
+
+
 REM Start the FastAPI application
 python main.py
